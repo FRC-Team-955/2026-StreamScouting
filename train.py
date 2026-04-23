4ce5d8f7c90753ef1a99c5323a547a69594a3a7d
fossil-fishhooks <arin.jander@student.csd509j.net>
2026-04-23 14:05:04 -0700

    dataset 2 and training device options

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8x.pt")
+model = YOLO("yolov8c.pt")
 
 model.train(
     data="dataset/data.yaml",
     epochs=50,
-    imgsz=640
+    imgsz=640,
+    device='cpu'  #device=0 for CUDA, device='mps' for apple silicon. remove this line for auto detect
 )
