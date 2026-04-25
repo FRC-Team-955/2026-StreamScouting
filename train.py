8fb398d7fd4d9f013ebea7978647c4f8998ab2a8
fossil-fishhhooks <arin.jander@student.csd509j.net>
2026-04-24 22:20:54 -0700

    second edition model train script

--- train.py
+++ train.py
@@ -1,15 +1,15 @@
 from ultralytics import YOLO
-
+# this is tuned for my system. Turn up batch until GPU memory full, turn up workers until RAM full
+# add cache=True if you have a lot of RAM
 def main():
-    model = YOLO("best.pt")
+    model = YOLO("yolov8m.pt") # change to best
     model.train(
-        data="dataset2/data.yaml",
-        epochs=50,
-        imgsz=640,
-        device=0, #  set to 'mps' on apple silicon, and 'cpu' otherwise. 0 means GPU
-        workers=18,
-        batch=24
-    )
-
+    data="dataset2/data.yaml",
+    epochs=190,
+    imgsz=640,
+    device=0,
+    workers=10,
+    batch=28,
+)
 if __name__ == "__main__":
     main()
